5cada0ce65545076fb2f65a7f94362617672fe85
Tung Nguyen <kenkz447@gmail.com>
2022-01-12 16:14:57 +0700

    first commit

---
+++ assets/bg-effect.js
@@ -0,0 +1,318 @@
+var words = ['javascript', 'typescript', 'SCSS', 'c#', 'reactJS', 'webpack', 'vueJS', 'netCore', 'flutter', 'dart', 'gitHub', 'jenkins', 'dockers', 'nginx', 'threeJS'];
+var colors = ['#007bff', '#6c757d', '#28a745', '#dc3545', '#ffc107', '#343a40'];
+
+var canvas = document.getElementById('bgEffect'),
+    can_w = parseInt(canvas.getAttribute('width')),
+    can_h = parseInt(canvas.getAttribute('height')),
+    ctx = canvas.getContext('2d');
+
+// console.log(typeof can_w);
+
+var ball = {
+    x: 0,
+    y: 0,
+    vx: 0,
+    vy: 0,
+    r: 0,
+    alpha: 1,
+    phase: 0
+},
+    ball_color = {
+        r: 200,
+        g: 200,
+        b: 200
+    },
+    R = .5,
+    balls = [],
+    alpha_f = 0.05,
+    alpha_phase = 0,
+
+    // Line
+    link_line_width = 0.8,
+    dis_limit = 260,
+    add_mouse_point = true,
+    mouse_in = false,
+    mouse_ball = {
+        x: 0,
+        y: 0,
+        vx: 0,
+        vy: 0,
+        r: 0,
+        type: 'mouse'
+    };
+
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+// Random speed
+function getRandomSpeed(pos) {
+    var min = -1,
+        max = 1;
+    switch (pos) {
+        case 'top':
+            return [randomNumFrom(min, max), randomNumFrom(0.1, max)];
+            break;
+        case 'right':
+            return [randomNumFrom(min, -0.1), randomNumFrom(min, max)];
+            break;
+        case 'bottom':
+            return [randomNumFrom(min, max), randomNumFrom(min, -0.1)];
+            break;
+        case 'left':
+            return [randomNumFrom(0.1, max), randomNumFrom(min, max)];
+            break;
+        default:
+            return;
+            break;
+    }
+}
+function randomArrayItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+function randomNumFrom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+// Random Ball
+function getRandomBall() {
+    var pos = randomArrayItem(['top', 'right', 'bottom', 'left']);
+    switch (pos) {
+        case 'top':
+            return {
+                x: randomSidePos(can_w),
+                y: -R,
+                vx: getRandomSpeed('top')[0],
+                vy: getRandomSpeed('top')[1],
+                r: R,
+                alpha: 1,
+                phase: randomNumFrom(0, 10),
+                word: getRandomWord(),
+                color: getRandomColor(),
+            }
+            break;
+        case 'right':
+            return {
+                x: can_w + R,
+                y: randomSidePos(can_h),
+                vx: getRandomSpeed('right')[0],
+                vy: getRandomSpeed('right')[1],
+                r: R,
+                alpha: 1,
+                phase: randomNumFrom(0, 10),
+                word: getRandomWord(),
+                color: getRandomColor(),
+            }
+            break;
+        case 'bottom':
+            return {
+                x: randomSidePos(can_w),
+                y: can_h + R,
+                vx: getRandomSpeed('bottom')[0],
+                vy: getRandomSpeed('bottom')[1],
+                r: R,
+                alpha: 1,
+                phase: randomNumFrom(0, 10),
+                word: getRandomWord(),
+                color: getRandomColor(),
+            }
+            break;
+        case 'left':
+            return {
+                x: -R,
+                y: randomSidePos(can_h),
+                vx: getRandomSpeed('left')[0],
+                vy: getRandomSpeed('left')[1],
+                r: R,
+                alpha: 1,
+                phase: randomNumFrom(0, 10),
+                word: getRandomWord(),
+                color: getRandomColor()
+            }
+            break;
+    }
+}
+function randomSidePos(length) {
+    return Math.ceil(Math.random() * length);
+}
+
+// Draw Ball
+function renderBalls() {
+    Array.prototype.forEach.call(balls, function (b) {
+        if (!b.hasOwnProperty('type')) {
+            ctx.fillStyle = 'rgba(' + ball_color.r + ',' + ball_color.g + ',' + ball_color.b + ',' + b.alpha + ')';
+            ctx.beginPath();
+            ctx.arc(b.x, b.y, R, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fill();
+
+            var width = ctx.measureText(b.word).width ?? 50;
+            var height = ctx.measureText(b.word).height ?? 20;
+
+            ctx.strokeStyle = b.color;
+            roundRect(ctx, b.x - 5, b.y - height / 2, width + 15, height);
+
+            ctx.fillStyle = 'rgba(' + ball_color.r + ',' + ball_color.g + ',' + ball_color.b + ',' + b.alpha + ')';
+            ctx.fillText(b.word, b.x + 2, b.y + 4);
+        }
+    });
+}
+
+// Update balls
+function updateBalls() {
+    var new_balls = [];
+    Array.prototype.forEach.call(balls, function (b) {
+        b.x += b.vx;
+        b.y += b.vy;
+
+        if (b.x > -(50) && b.x < (can_w + 50) && b.y > -(50) && b.y < (can_h + 50)) {
+            new_balls.push(b);
+        }
+
+        // alpha change
+        b.phase += alpha_f;
+        b.alpha = Math.abs(Math.cos(b.phase));
+        // console.log(b.alpha);
+    });
+
+    balls = new_balls.slice(0);
+}
+
+// loop alpha
+function loopAlphaInf() {
+
+}
+
+// Draw lines
+function renderLines() {
+    var fraction, alpha;
+
+    for (var i = 0; i < balls.length; i++) {
+        fraction = getDisOf(balls[i], mouse_ball) / dis_limit;
+        if (fraction < 1) {
+            alpha = (1 - fraction).toString();
+
+            ctx.strokeStyle = 'rgba(255,255,255,' + alpha + ')';
+            ctx.lineWidth = link_line_width;
+
+            ctx.beginPath();
+            ctx.moveTo(balls[i].x, balls[i].y);
+            ctx.lineTo(mouse_ball.x, mouse_ball.y);
+            ctx.stroke();
+            ctx.closePath();
+        }
+    }
+}
+
+// calculate distance between two points
+function getDisOf(b1, b2) {
+    var delta_x = Math.abs(b1.x - b2.x),
+        delta_y = Math.abs(b1.y - b2.y);
+
+    return Math.sqrt(delta_x * delta_x + delta_y * delta_y);
+}
+
+// add balls if there a little balls
+function addBallIfy() {
+    if (balls.length < 20) {
+        balls.push(getRandomBall());
+    }
+}
+
+// Render
+function render() {
+    ctx.clearRect(0, 0, can_w, can_h);
+
+    renderBalls();
+
+    renderLines();
+
+    updateBalls();
+
+    addBallIfy();
+
+    window.requestAnimationFrame(render);
+}
+
+// Init Balls
+function initBalls(num) {
+    for (var i = 1; i <= num; i++) {
+        balls.push({
+            x: randomSidePos(can_w),
+            y: randomSidePos(can_h),
+            vx: getRandomSpeed('top')[0],
+            vy: getRandomSpeed('top')[1],
+            r: R,
+            alpha: 1,
+            phase: randomNumFrom(0, 10),
+            word: getRandomWord(),
+            color: getRandomColor(),
+        });
+    }
+}
+// Init Canvas
+function initCanvas() {
+    canvas.setAttribute('width', window.innerWidth);
+    canvas.setAttribute('height', window.innerHeight);
+
+    can_w = parseInt(canvas.getAttribute('width'));
+    can_h = parseInt(canvas.getAttribute('height'));
+}
+window.addEventListener('resize', function (e) {
+    console.log('Window Resize...');
+    initCanvas();
+});
+
+function goMovie() {
+    initCanvas();
+    initBalls(60);
+    window.requestAnimationFrame(render);
+}
+goMovie();
+
+balls.push(mouse_ball);
+
+window.addEventListener('mousemove', function (e) {
+    var e = e || window.event;
+    mouse_ball.x = e.pageX;
+    mouse_ball.y = e.pageY;
+    // console.log(mouse_ball);
+});
+
+function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
+    if (typeof stroke === 'undefined') {
+        stroke = true;
+    }
+    if (typeof radius === 'undefined') {
+        radius = 5;
+    }
+    if (typeof radius === 'number') {
+        radius = { tl: radius, tr: radius, br: radius, bl: radius };
+    } else {
+        var defaultRadius = { tl: 0, tr: 0, br: 0, bl: 0 };
+        for (var side in defaultRadius) {
+            radius[side] = radius[side] || defaultRadius[side];
+        }
+    }
+    ctx.beginPath();
+    ctx.moveTo(x + radius.tl, y);
+    ctx.lineTo(x + width - radius.tr, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+    ctx.lineTo(x + width, y + height - radius.br);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+    ctx.lineTo(x + radius.bl, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+    ctx.lineTo(x, y + radius.tl);
+    ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+    ctx.closePath();
+    if (fill) {
+        ctx.fill();
+    }
+    if (stroke) {
+        ctx.stroke();
+    }
+}
